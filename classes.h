f7632ac8b99154d5ef5bda76d0eed506c5bed067
IKRAM-abdelkefi <95529811+IKRAM-abdelkefi@users.noreply.github.com>
2023-03-28 02:29:15 +0100

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,25 +1,25 @@
-#ifndef CLASSES_H_INCLUDED
-#define CLASSES_H_INCLUDED
-
+#ifndef CLASS_H_INCLUDED
+#define CLASS_H_INCLUDED
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <functional>
 using namespace std;
-
 class point
 {
     float x ;
     float y ;
     point(float a, float b): x(a), y(b) {};
+    point(const point& A):x(A.x), y(A.y){};
 };
-class graphe ;
+class graphe
 {
 public :
     int dim;
-    double** tab;
-    graphe( int n )
+    int** tab;
+    graphe( int n );
     ~graphe();
-    double val (int i , int j) {return tab[i][j]};
+    int val (int i , int j) {return tab[i][j];};
     void affiche();
 };
 
@@ -28,40 +28,56 @@ class ville
 public:
     string nom;
     point coord;
-    ville (string a , point  p ) : nom(a), coord (p){};
+    //ville (string a ,const point&  p ) : nom(a), coord (p){};
 
 };
+
 
 class individu
 {
 public:
     int dim;
-    double* indiv;
-    double* poids;
+    int* indiv;
     individu(int n);
-    virtual individu (individu v)
-    ~individu();
-    virtual double adapt();
+    individu  (const individu& v);
+    individu()=default ;// default constructor
+    virtual~individu(){};
+    virtual int adapt(graphe G) const =0;
 
 };
 class chemin : public individu
 {
 public:
-    chemin(int n) : individu (n) {};
-    chemin (chemin v);
-    double adapt ();
-
+    chemin(): individu(){};
+    chemin(int n) : individu(n) {} ;
+    chemin (const chemin& other) : individu(other) {};
+    int adapt (graphe G) const;
+    ~chemin();
+    void affiche();
 };
 class population
 {
 public:
     int nbre;
-    chemin* pop;
+    vector <chemin> pop;
     population (int n );
-    chemin operator[] (int i ) {return chemin[i] };
+    population(const population& other);
+    ~population();
+    chemin& operator[] (int i ) {return pop.at(i);};
 
 };
 chemin mutation (chemin ch)  ;
-population* selection_prod (pipulation pop, string select);
+// méthodes de séléction des reproducteurs
+chemin selec_roulette(population pop, graphe G);
+chemin selec_rang(const population& pop, graphe G );
+population selec_tournoi(const population &pop,graphe G);
 
 
+// méthode de sélection de next generation
+bool compare_by_adapt_asc(const chemin& A,const chemin& B, graphe G);
+bool compare_by_adapt_desc(const chemin& A,const chemin& B, graphe G);
+population selec_reproducteurs(population pop_initi,graphe G, std::function<chemin(const population&, graphe)> selection_method);
+population selection_nextgen(population pop_prod, int q, graphe G);
+
+
+#endif CLASS_H_INCLUDED
